d488f10d225001486b605d040ff8e09341679970
phatcharanun <153785070+phatcharanun@users.noreply.github.com>
2024-01-04 11:59:08 +0700

    Maze_

---
+++ Maze.py
@@ -0,0 +1,183 @@
+import os
+import keyboard
+import time
+
+class maze:
+    def __init__(self) -> None:
+        self.maze = [
+                    ["X", "X", "X", "X", "X", "X", "X"],
+                    ["X", " ", " ", " ", "X", " ", "X"],
+                    ["X", " ", "X", " ", "X", " ", " "],
+                    ["X", " ", "X", " ", "X", " ", "X"],
+                    ["X", " ", "X", " ", " ", " ", "X"],
+                    ["X", " ", "X", "X", "X", "X", "X"],
+                    ]
+        self.ply = pos(5, 1)
+        self.end = pos(2, 6)
+        self.maze[self.ply.y][self.ply.x] = "P"
+        self.maze[self.end.y][self.end.x] = "E"
+    
+    def isInBound(self, y, x):
+        if y>=0 and x>=0 and y<len(self.maze) and x<len(self.maze[0]):
+            return True
+        else:
+            return False
+    
+    def print(self):
+        os.system("cls")
+        print("\n\n\n")
+        for row in self.maze:
+            for col in row:
+                print(col," ", end="")
+            print("")
+        print("\n\n\n")
+    
+    def printEND(self):
+        os.system("cls")
+        print("\n\n\n")
+        print(">>>>> Congraturation!!! <<<<<")
+        print("\n\n\n")
+        keyboard.wait("")
+
+    def move_up(self):
+        next_move = pos(self.ply.y-1, self.ply.x)
+        if self.isInBound(next_move.y,next_move.x):
+            if self.maze[next_move.y][next_move.x] == " ":
+                self.maze[self.ply.y][self.ply.x] = " "
+                self.maze[next_move.y][next_move.x] = "P"
+                self.ply = next_move
+                time.sleep(0.25)
+            if self.maze[next_move.y][next_move.x] == "E":
+                self.printEND()
+                return False
+        return True
+    
+    def move_down(self):
+        next_move = pos(self.ply.y+1, self.ply.x)
+        if self.isInBound(next_move.y,next_move.x):
+            if self.maze[next_move.y][next_move.x] == " ":
+                self.maze[self.ply.y][self.ply.x] = " "
+                self.maze[next_move.y][next_move.x] = "P"
+                self.ply = next_move
+                time.sleep(0.25)
+            if self.maze[next_move.y][next_move.x] == "E":
+                self.printEND()
+                return False
+        return True
+
+    def move_left(self):
+        next_move = pos(self.ply.y, self.ply.x-1)
+        if self.isInBound(next_move.y,next_move.x):
+            if self.maze[next_move.y][next_move.x] == " ":
+                self.maze[self.ply.y][self.ply.x] = " "
+                self.maze[next_move.y][next_move.x] = "P"
+                self.ply = next_move
+                time.sleep(0.25)
+            if self.maze[next_move.y][next_move.x] == "E":
+                self.printEND()
+                return False
+        return True
+
+    def move_right(self):
+        next_move = pos(self.ply.y, self.ply.x+1)
+        if self.isInBound(next_move.y,next_move.x):
+            if self.maze[next_move.y][next_move.x] == " ":
+                self.maze[self.ply.y][self.ply.x] = " "
+                self.maze[next_move.y][next_move.x] = "P"
+                self.ply = next_move
+                time.sleep(0.25)
+            if self.maze[next_move.y][next_move.x] == "E":
+                self.printEND()
+                return False
+        return True
+
+class pos:
+    def __init__(self) -> None:
+        self.y = None
+        self.x = None
+    
+    def __init__(self, y, x) -> None:
+        self.y = y
+        self.x = x
+
+
+class Stack:
+
+    def __init__(self):
+        self._top = None
+        self._size = 0
+
+    def isEmpty(self):
+        return self._top is None
+    
+    def __len__(self):
+        return self._size
+    
+    def peek(self):
+        assert not self.isEmpty(), "Cannot peek at an empty stack"
+        return self._top.item
+
+    def pop(self):
+        assert not self.isEmpty(), "Cannot pop from an empty stack"
+        node = self._top
+        self._top = self._top.next
+        self._size = self._size - 1
+        return node.item
+
+    def push(self, item):
+        self._top = _StackNode(item, self._top)
+        self._size = self._size + 1
+
+class _StackNode:
+    def __init__(self, item, link):
+        self.item = item
+        self.next = link
+
+class Queue:
+    def __init__(self):
+        self._qList = list()
+
+    def isEmpty(self):
+        return len(self) == 0
+    
+    def __len__(self):
+        return len(self._qList)
+    
+    def enqueue(self, item):
+        self._qList.append(item)
+
+    def dequeue(self):
+        assert not self.isEmpty(), "Cannot dequeue from an empty queue."
+        return self._qList.pop(0)
+
+
+if __name__ == '__main__':
+
+    m = maze()
+    m.print()
+    while True:
+        if keyboard.is_pressed("q"):
+            print("Quit Program")
+            break
+        if keyboard.is_pressed("w"):
+            if m.move_up():
+                m.print()
+            else:
+                break
+        if keyboard.is_pressed("s"):
+            if m.move_down():
+                m.print()
+            else:
+                break
+        if keyboard.is_pressed("a"):
+            if m.move_left():
+                m.print()
+            else:
+                break
+        if keyboard.is_pressed("d"):
+            if m.move_right():
+                m.print()
+            else:
+                break
+
+#TEST
